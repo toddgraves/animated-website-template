8132ecf74bd0ec5661f4784462723fa47bb96ba9
toddgraves <todd@accessvsc.com>
2020-11-26 11:52:37 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// AOS.js
+  AOS.init({
+    delay: 200, // Values from 0 to 3000, in steps of 50ms
+    duration: 1500, // Values from 0 to 3000, in steps of 50ms
+    once: false, // whether animation should happen once while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+  });
